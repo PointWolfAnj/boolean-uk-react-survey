4ecaa59d90fc491306c0df3ccdafa5257183fc99
PointWolfAnj <anojan31@gmail.com>
2022-03-12 01:03:19 +0000

    ext

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,200 +1,208 @@
 import { useState } from "react"
+import AnswersList from "./AnswersList"
 
 function Main() {
   const [open, setOpen] = useState(false) //Ignore this state
-  const [rate, setRate] = useState("")
-  const [time, setTime] = useState("")
-  const [say, setSay] = useState("")
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
 
-  console.log("Radio Data", rate)
-  console.log("Check Data", time)
-  console.log("Input Data", say, username, email)
+  const [submittedData, setSubmittedData] = useState([])
+  const [editing, setEditing] = useState(0)
+  const [id, setId] = useState(1)
 
-  function rateRadioInput(event) {
-    const inputValue = event.target.value
-    setRate(inputValue)
+  const resetForm = {
+    id: id,
+    color: "",
+    spendTime: {
+      swimming: false,
+      bathing: false,
+      chatting: false,
+      noTime: false,
+    },
+    review: "",
+    name: "",
+    email: "",
   }
 
-  function checkTimeInput(event) {
-    const inputValue = event.target.value
-    setTime(inputValue)
+  const [duckData, setDuckData] = useState(resetForm)
+
+  const handleSpendTime = (event) => {
+    setDuckData({
+      ...duckData,
+      spendTime: {
+        ...duckData.spendTime,
+        [event.target.value]: event.target.checked,
+      },
+    })
   }
 
-  function sayInput(event) {
-    const inputValue = event.target.value
-
-    setSay(inputValue)
+  const handleInput = (event) => {
+    setDuckData({ ...duckData, [event.target.name]: event.target.value })
   }
 
-  function usernameInput(event) {
-    const inputValue = event.target.value
-    setUsername(inputValue)
-  }
-
-  function emailInput(event) {
-    const inputValue = event.target.value
-    setEmail(inputValue)
-  }
-
-  function submitButton(event) {
+  const handleSubmit = (event) => {
+    console.log(duckData)
     event.preventDefault()
-    const userInfo = { rate, time, say, username, email }
-    console.log("Form Submission", userInfo)
-
-    setRate("")
-    setTime("")
-    setSay("")
-    setUsername("")
-    setEmail("")
+    if (editing === 0) {
+      setSubmittedData([...submittedData, duckData])
+      setId(id + 1)
+    } else if (editing !== 0) {
+      const updatedData = submittedData.map((item) =>
+        item.id === editing ? duckData : item
+      )
+      setSubmittedData(updatedData)
+      setEditing(0)
+    }
+    setDuckData({ ...resetForm, id: id + 1 })
   }
 
   return (
-    <main className="main" onSubmit={submitButton}>
+    <main className="main">
       <section className={`main__list ${open ? "open" : ""}`}>
         <h2>Answers list</h2>
-        {/* answers should go here */}
+        <AnswersList
+          submittedData={submittedData}
+          setDuckData={setDuckData}
+          setEditing={setEditing}
+        />
       </section>
       <section className="main__form">
-        {
-          <form class="form">
-            <h2>Tell us what you think about your rubber duck!</h2>
-            <div class="form__group radio">
-              <h3>How do you rate your rubber duck colour?</h3>
-              <ul>
-                <li>
+        <form class="form" onSubmit={handleSubmit}>
+          <h2>Tell us what you think about your rubber duck!</h2>
+          <div class="form__group radio">
+            <h3>How do you rate your rubber duck colour?</h3>
+            <ul>
+              <li>
+                <input
+                  id="color-one"
+                  type="radio"
+                  name="color"
+                  value="1"
+                  onChange={handleInput}
+                  checked={duckData.color === "1"}
+                />
+                <label for="color-one">1</label>
+              </li>
+              <li>
+                <input
+                  id="color-two"
+                  type="radio"
+                  name="color"
+                  value="2"
+                  onChange={handleInput}
+                  checked={duckData.color === "2"}
+                />
+                <label for="color-two">2</label>
+              </li>
+              <li>
+                <input
+                  id="color-three"
+                  type="radio"
+                  name="color"
+                  value="3"
+                  onChange={handleInput}
+                  checked={duckData.color === "3"}
+                />
+                <label for="color-three">3</label>
+              </li>
+              <li>
+                <input
+                  id="color-four"
+                  type="radio"
+                  name="color"
+                  value="4"
+                  onChange={handleInput}
+                  checked={duckData.color === "4"}
+                />
+                <label for="color-four">4</label>
+              </li>
+            </ul>
+          </div>
+          <div class="form__group">
+            <h3>How do you like to spend time with your rubber duck</h3>
+            <ul>
+              <li>
+                <label>
                   <input
-                    id="color-one"
-                    type="radio"
-                    name="color"
-                    value="1"
-                    onChange={rateRadioInput}
-                    checked={rate === "1"}
+                    name="spend-time"
+                    type="checkbox"
+                    value="swimming"
+                    onChange={handleSpendTime}
+                    checked={duckData.spendTime.swimming}
                   />
-                  <label for="color-one">1</label>
-                </li>
-                <li>
+                  Swimming
+                </label>
+              </li>
+              <li>
+                <label>
                   <input
-                    id="color-two"
-                    type="radio"
-                    name="color"
-                    value="2"
-                    onChange={rateRadioInput}
-                    checked={rate === "2"}
+                    name="spend-time"
+                    type="checkbox"
+                    value="bathing"
+                    onChange={handleSpendTime}
+                    checked={duckData.spendTime.bathing}
                   />
-                  <label for="color-two">2</label>
-                </li>
-                <li>
+                  Bathing
+                </label>
+              </li>
+              <li>
+                <label>
                   <input
-                    id="color-three"
-                    type="radio"
-                    name="color"
-                    value="3"
-                    onChange={rateRadioInput}
-                    checked={rate === "3"}
+                    name="spend-time"
+                    type="checkbox"
+                    value="chatting"
+                    onChange={handleSpendTime}
+                    checked={duckData.spendTime.chatting}
                   />
-                  <label for="color-three">3</label>
-                </li>
-                <li>
+                  Chatting
+                </label>
+              </li>
+              <li>
+                <label>
                   <input
-                    id="color-four"
-                    type="radio"
-                    name="color"
-                    value="4"
-                    onChange={rateRadioInput}
-                    checked={rate === "4"}
+                    name="spend-time"
+                    type="checkbox"
+                    value="noTime"
+                    onChange={handleSpendTime}
+                    checked={duckData.spendTime.noTime}
                   />
-                  <label for="color-four">4</label>
-                </li>
-              </ul>
-            </div>
-            <div class="form__group">
-              <h3>How do you like to spend time with your rubber duck</h3>
-              <ul>
-                <li>
-                  <label>
-                    <input
-                      name="spend-time"
-                      type="checkbox"
-                      value="swimming"
-                      onChange={checkTimeInput}
-                      checked={time === "swimming"}
-                    />
-                    Swimming
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input
-                      name="spend-time"
-                      type="checkbox"
-                      value="bathing"
-                      onChange={checkTimeInput}
-                      checked={time === "bathing"}
-                    />
-                    Bathing
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input
-                      name="spend-time"
-                      type="checkbox"
-                      value="chatting"
-                      onChange={checkTimeInput}
-                      checked={time === "chatting"}
-                    />
-                    Chatting
-                  </label>
-                </li>
-                <li>
-                  <label>
-                    <input
-                      name="spend-time"
-                      type="checkbox"
-                      value="noTime"
-                      onChange={checkTimeInput}
-                      checked={time === "noTime"}
-                    />
-                    I don't like to spend time with it
-                  </label>
-                </li>
-              </ul>
-            </div>
-            <label>
-              What else have you got to say about your rubber duck?
-              <textarea
-                name="review"
-                cols="30"
-                rows="10"
-                onChange={sayInput}
-                value={say}
-              ></textarea>
-            </label>
-            <label>
-              Put your name here (if you feel like it):
-              <input
-                type="text"
-                name="username"
-                value=""
-                onChange={usernameInput}
-                value={username}
-              />
-            </label>
-            <label>
-              Leave us your email pretty please??
-              <input
-                type="email"
-                name="email"
-                value=""
-                onChange={emailInput}
-                value={email}
-              />
-            </label>
-            <input class="form__submit" type="submit" value="Submit Survey!" />
-          </form>
-        }
+                  I don't like to spend time with it
+                </label>
+              </li>
+            </ul>
+          </div>
+          <label>
+            What else have you got to say about your rubber duck?
+            <textarea
+              name="review"
+              cols="30"
+              rows="10"
+              value={duckData.review}
+              onChange={handleInput}
+            ></textarea>
+          </label>
+          <label>
+            Put your name here (if you feel like it):
+            <input
+              type="text"
+              name="name"
+              value={duckData.name}
+              onChange={handleInput}
+            />
+          </label>
+          <label>
+            Leave us your email pretty please??
+            <input
+              type="email"
+              name="email"
+              value={duckData.email}
+              onChange={handleInput}
+            />
+          </label>
+          <input
+            class="form__submit"
+            type="submit"
+            value={editing !== 0 ? "Edit Answer" : "Submit Survey!"}
+          />
+        </form>
       </section>
     </main>
   )
